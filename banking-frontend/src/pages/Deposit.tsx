2915e5ffdcbf268167b9916ffc183b06d103f317
Jamersom Silva <jamersom.silva@outlook.com>
2025-12-17 06:21:15 -0300

    adicionando toast no deposit

--- banking-frontend/src/pages/Deposit.tsx
+++ banking-frontend/src/pages/Deposit.tsx
@@ -2,10 +2,17 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { deposit } from '../services/operationService';
 import { getAccounts } from '../services/accountService';
+import Toast from '../components/Toast';
 
 export default function Deposit() {
   const [accountId, setAccountId] = useState<string | null>(null);
   const [amount, setAmount] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [toast, setToast] = useState<{
+    message: string;
+    type: 'success' | 'error';
+  } | null>(null);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,7 +20,6 @@ export default function Deposit() {
       const accounts = await getAccounts();
       setAccountId(accounts[0].id);
     }
-
     loadAccount();
   }, []);
 
@@ -21,22 +27,38 @@ export default function Deposit() {
     e.preventDefault();
     if (!accountId) return;
 
-    await deposit(accountId, { amount: Number(amount) });
-    navigate('/');
+    setLoading(true);
+
+    try {
+      await deposit(accountId, { amount: Number(amount) });
+      setToast({ message: 'Depósito realizado com sucesso!', type: 'success' });
+
+      setTimeout(() => navigate('/'), 1000);
+    } catch {
+      setToast({ message: 'Erro ao realizar depósito', type: 'error' });
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Depósito</h1>
+    <>
+      {toast && <Toast message={toast.message} type={toast.type} />}
 
-      <input
-        type="number"
-        min="1"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <h1>Depósito</h1>
 
-      <button>Depositar</button>
-    </form>
+        <input
+          type="number"
+          min="1"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+
+        <button disabled={loading}>
+          {loading ? 'Processando...' : 'Depositar'}
+        </button>
+      </form>
+    </>
   );
 }
