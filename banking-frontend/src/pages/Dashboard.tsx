44e535ad52a49a9ad96362a8442f1dd8dfcb66d9
Jamersom Silva <jamersom.silva@outlook.com>
2025-12-17 05:49:45 -0300

    substituindo mocks

--- banking-frontend/src/pages/Dashboard.tsx
+++ banking-frontend/src/pages/Dashboard.tsx
@@ -1,37 +1,49 @@
+import { useEffect, useState } from 'react';
 import DashboardLayout from '../layouts/DashboardLayout';
 import StatCard from '../components/StatCard';
 import TransactionsList from '../components/TransactionsList';
-import { dashboardData, transactionsData } from '../mocks/data';
 import BalanceChart from '../components/BalanceChart';
 
+import { getAccounts, getBalance, getStatement } from '../services/accountService';
+import type { BankTransaction } from '../types/Transaction';
 
 export default function Dashboard() {
+  const [balance, setBalance] = useState(0);
+  const [transactions, setTransactions] = useState<BankTransaction[]>([]);
+
+  useEffect(() => {
+    async function loadData() {
+      const accounts = await getAccounts();
+      const accountId = accounts[0].id;
+
+      const balance = await getBalance(accountId);
+      const statement = await getStatement(accountId);
+
+      setBalance(balance);
+      setTransactions(statement);
+    }
+
+    loadData();
+  }, []);
+
   return (
     <DashboardLayout>
       <h1 className="text-2xl font-bold mb-6">Dashboard</h1>
 
       <div className="grid grid-cols-4 gap-6 mb-8">
-        <StatCard title="Saldo atual" value={`R$ ${dashboardData.balance.toFixed(2)}`} />
-        <StatCard title="Entradas" value={`R$ ${dashboardData.deposits.toFixed(2)}`} />
-        <StatCard title="Saídas" value={`R$ ${dashboardData.withdrawals.toFixed(2)}`} />
-        <StatCard title="Saldo disponível" value={`R$ ${dashboardData.availableBalance.toFixed(2)}`} />
+        <StatCard title="Saldo atual" value={`R$ ${balance.toFixed(2)}`} />
+        <StatCard title="Entradas" value="—" />
+        <StatCard title="Saídas" value="—" />
+        <StatCard title="Saldo disponível" value={`R$ ${balance.toFixed(2)}`} />
       </div>
 
-    <div className="grid grid-cols-3 gap-6 mb-8">
-  <div className="col-span-2">
-    <BalanceChart />
-  </div>
+      <div className="grid grid-cols-3 gap-6">
+        <div className="col-span-2">
+          <BalanceChart />
+        </div>
 
-  <div>
-    <h2 className="text-xl font-semibold mb-4">Extrato</h2>
-    <TransactionsList transactions={transactionsData} />
-  </div>
-</div>
-
-
-      <h2 className="text-xl font-semibold mb-4">Extrato</h2>
-
-      <TransactionsList transactions={transactionsData} />
+        <TransactionsList transactions={transactions} />
+      </div>
     </DashboardLayout>
   );
 }
