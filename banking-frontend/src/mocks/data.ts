bc9172db1fca3720efa4e6dca51878de51fde1d5
Jamersom Silva <jamersom.silva@outlook.com>
2025-12-17 05:29:58 -0300

    ajustando o data.ts para usat o tyoe mokado

--- banking-frontend/src/mocks/data.ts
+++ banking-frontend/src/mocks/data.ts
@@ -1,13 +1,15 @@
+import { Transaction } from '../types/Transaction';
+
 export const dashboardData = {
-  balance: 1250.50,
-  deposits: 3200.00,
-  withdrawals: 1950.00,
-  availableBalance: 1250.50,
+  balance: 1250.5,
+  deposits: 3200.0,
+  withdrawals: 1950.0,
+  availableBalance: 1250.5,
 };
 
-export const transactionsData = [
-  { id: 1, type: "DEPOSIT", amount: 1000, date: "2025-12-15" },
-  { id: 2, type: "WITHDRAW", amount: 500, date: "2025-12-14" },
-  { id: 3, type: "DEPOSIT", amount: 1200, date: "2025-12-10" },
-  { id: 4, type: "WITHDRAW", amount: 450, date: "2025-12-09" },
+export const transactionsData: Transaction[] = [
+  { id: 1, type: 'DEPOSIT', amount: 1000, date: '2025-12-15' },
+  { id: 2, type: 'WITHDRAW', amount: 500, date: '2025-12-14' },
+  { id: 3, type: 'DEPOSIT', amount: 1200, date: '2025-12-10' },
+  { id: 4, type: 'WITHDRAW', amount: 450, date: '2025-12-09' },
 ];
